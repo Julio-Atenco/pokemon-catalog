1b0008c3cd8fbf1d6d547312f8fbe340c15acc75
Julio-Atenco <atencoaguilarjulio.c@gmail.com>
2026-02-05 06:23:40 +0000

    Remove output configuration from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/pokemon-catalog' : '',
   images: {
     unoptimized: true,
